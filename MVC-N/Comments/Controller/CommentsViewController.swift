568164202e2c3c7db4cb3995e153482af900ce66
Artur <artur.anissimov@gmail.com>
2020-03-24 21:17:32 +0200

    Project setting.

---
+++ MVC-N/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Artur Anissimov on 24.03.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+
+}
+
+extension CommentsViewController: UITableViewDelegate { }
+
+extension CommentsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+}
